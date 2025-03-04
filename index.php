995d7d71838d35c229748135809c3a7ab48d288a
Bagtiyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-03-04 14:03:57 +0500

    1.0.0

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $name = "Bagtiyar";
+    $salary = 230.0;
+    $experience_months = 30;
+    $employed = true;
+    echo "\${$salary} <br> {$employed} <br>";
+    echo "Hello {$name}";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    this is me
+</body>
+</html>
