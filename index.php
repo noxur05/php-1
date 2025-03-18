ca37da6668d6321be83a496840f2f4bfde9a668c
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-03-19 00:14:13 +0500

    formatted

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <form action="index.php" method="post">
-        
-    </form>
-    <?php 
-        $host = "localhost";
-        $port = "5432";
-        $dbname = "test";
-        $user = "postgres";
-        $password = "admin";
+    <body>
+        <form action="index.php" method="post">
 
-        try {
-            $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+        </form>
+        <?php
+            $host = "localhost";
+            $port = "5432";
+            $dbname = "test";
+            $user = "postgres";
+            $password = "admin";
 
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
-        } catch(PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
-        }
-    ?>
-</body>
+            try {
+                $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Connected successfully";
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+        ?>
+    </body>
 </html>
