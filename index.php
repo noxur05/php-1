112a37dd182870f2857e3aafe4d54c66ef9dc84c
Bagtyyar Rejepov <rejepowbagtyyar05@gmail.com>
2025-03-18 23:57:48 +0500

    connected db to php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php
-    $name = "Bagtiyar";
-    $salary = 230.0;
-    $experience_months = 30;
-    $employed = true;
-    echo "\${$salary} <br> {$employed} <br>";
-    echo "Hello {$name}";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,6 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    this is me
+    <form action="index.php" method="post">
+        
+    </form>
+    <?php 
+        $host = "localhost";
+        $port = "5432";
+        $dbname = "test";
+        $user = "postgres";
+        $password = "admin";
+
+        try {
+            $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    ?>
 </body>
 </html>
